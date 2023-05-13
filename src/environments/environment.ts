725162981f9e5a577ee873dd0c67ac7fd3bd9536
codexpression-nj <n.codexpression@gmail.com>
2023-05-13 23:29:57 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
    firebaseConfig : {
-    apiKey: "AIzaSyCUKdlTgeEH_CcVpDZQ387vCKPm2C8qzSs",
+    apiKey: "",
     authDomain: "auth-test-7b9fb.firebaseapp.com",
     projectId: "auth-test-7b9fb",
     storageBucket: "auth-test-7b9fb.appspot.com",
     messagingSenderId: "909454782709",
-    appId: "1:909454782709:web:70ca68de07cf04e84fd5bc",
+    appId: "",
     measurementId: "G-8TMKGEFEKD"
   }
 };
