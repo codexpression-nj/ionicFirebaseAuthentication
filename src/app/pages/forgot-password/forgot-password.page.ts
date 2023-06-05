cef30fe283d03ef4b4bcdb8445f9271babf903f5
codexpression-nj <n.codexpression@gmail.com>
2023-06-05 22:25:30 +0200

    update

--- src/app/pages/forgot-password/forgot-password.page.ts
+++ src/app/pages/forgot-password/forgot-password.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from 'src/app/auth-service.service';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,7 +10,7 @@ import { AuthServiceService } from 'src/app/auth-service.service';
 })
 export class ForgotPasswordPage implements OnInit {
 email:any
-  constructor(private authService:AuthServiceService) { }
+  constructor(private authService:AuthServiceService,private toastController: ToastController,private router: Router) { }
 
   ngOnInit() {
   }
@@ -16,7 +18,19 @@ email:any
   reset(){
     this.authService.resetPassword(this.email).then( () =>{      
       console.log('sent'); //show confirmation dialog
+      this.presentToast()
     })
   }
-
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your reset password link has been sent on your email',
+      duration: 2000, // Duration in milliseconds
+      position: 'bottom' // Position of the toast (top, bottom, middle)
+    });
+  
+    toast.present();
+    toast.onDidDismiss().then(()=>{
+      this.router.navigate(['/login']);
+    })
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
      firebaseConfig : {
-    apiKey: "AIzaSyCUKdlTgeEH_CcVpDZQ387vCKPm2C8qzSs",
+    apiKey: "",
     authDomain: "auth-test-7b9fb.firebaseapp.com",
     projectId: "auth-test-7b9fb",
     storageBucket: "auth-test-7b9fb.appspot.com",
