af1b3fc96b50795d668db01929c5f474b543c507
codexpression-nj <n.codexpression@gmail.com>
2023-06-22 22:45:18 +0200

    update

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthServiceService } from 'src/app/auth-service.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,27 +9,54 @@ import { AuthServiceService } from 'src/app/auth-service.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  
-  email:any
-  password:any
-  contact:any
+  ionicForm: FormGroup;
 
-  constructor(private authService:AuthServiceService,private router: Router) { }
+  // email:any
+  // password:any
+  // contact:any
+
+  constructor(private authService: AuthServiceService, private router: Router, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
-
+    this.ionicForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
+      password: ['n', [
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}'),
+        Validators.required,
+      ]
+    ],
+    });
   }
 
-  login(){
-    console.log(this.email + this.password);
-    
-    this.authService.loginUser(this.email,this.password).then((resp)=>{
-     
-        if(resp.user){
-          this.router.navigate(['/home'])
-        }
-        
-    })
+  login() {
+    // console.log(this.email + this.password);
+
+    // this.authService.loginUser(this.email,this.password).then((resp)=>{
+
+    //     if(resp.user){
+    //       this.router.navigate(['/home'])
+    //     }
+
+    // })
   }
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+  submitForm = () => {
+    if (this.ionicForm.valid) {
+      console.log("oh no");
+
+      console.log(this.ionicForm.value);
+      return false;
+    } else {
+      return console.log('Please provide all the required values!');
+    }
+  };
 
 }
